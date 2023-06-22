b5d16bdc727b1b6fc44261c034e1515b691f5760
Lucas Martins <lucas.martins.fsa@gmail.com>
2023-06-22 17:26:35 -0300

    estruturação do projeto. configuração do docker-compose para uso de containner para o banco de dados mysql

---
+++ src/main/java/com/devlucasmart/pedido_produtos_api/PedidoProdutosApiApplication.java
@@ -0,0 +1,13 @@
+package com.devlucasmart.pedido_produtos_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class PedidoProdutosApiApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(PedidoProdutosApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devlucasmart/pedido_produtos_api/PedidoProdutosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devlucasmart.pedido_produtos_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PedidoProdutosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
